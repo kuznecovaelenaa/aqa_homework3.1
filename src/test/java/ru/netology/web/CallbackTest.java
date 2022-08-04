ce065a5909e0be0e844c1d8ee822c8c4ad7f8c50
Elena <kuznecovaelenaa@yandex.ru>
2022-08-04 18:47:55 +0300

    first commit

---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,37 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.SQLOutput;
+
+public class CallbackTest {
+
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void firstTest() {
+        driver.get("http://localhost:9999");
+        System.out.println();
+    }
+}
