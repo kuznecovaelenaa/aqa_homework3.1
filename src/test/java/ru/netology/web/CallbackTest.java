84dc2d826daae29a4d80ad3430c8adae4b881df6
Elena <kuznecovaelenaa@yandex.ru>
2022-08-05 13:28:24 +0300

    chander test#3

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -29,7 +29,7 @@ public class CallbackTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
 
